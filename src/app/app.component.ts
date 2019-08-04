4ae24d34c86d5b50ef2ede0d2908406b02c75ec1
anhnt2989 <anhnt2989@gmail.com>
2019-08-05 00:19:22 +0700

    completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-service-countLifeTime';
+  constructor(private router: Router) {}
+  gotoTimelines() {
+    this.router.navigate(['/timelines']);
+  }
+}
