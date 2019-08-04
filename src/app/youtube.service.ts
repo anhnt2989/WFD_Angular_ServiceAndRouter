ffbc0f67a024ef16616ac5a1b38fb87922edde18
anhnt2989 <anhnt2989@gmail.com>
2019-08-05 00:56:02 +0700

    add more to youtube playlist

--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -9,7 +9,11 @@ export class YoutubeService {
     {id: 'HBYirj2c_jw', song: 'Deadpool 2 - Take on Me'},
     {id: 'N6O2ncUKvlg', song: 'Nelly - Just A Dream'},
     {id: 'uelHwf8o7_U', song: 'Eminem - Love The Way You Lie ft. Rihanna'},
-    {id: 'WpYeekQkAdc', song: 'The Black Eyed Peas - Where Is The Love?'}
+    {id: 'NmugSMBh_iI', song: 'Maroon 5 - Maps'},
+    {id: 'fwK7ggA3-bU', song: 'Maroon 5 - One More Night'},
+    {id: '09R8_2nJtjg', song: 'Maroon 5 - Sugar'},
+    {id: '3O1_3zBUKM8', song: 'Naughty Boy - La la la ft. Sam Smith (Official Video)'},
+    {id: 'mWRsgZuwf_8', song: 'Imagine Dragons - Demons (Official Video)'}
   ];
   constructor() { }
   find(id: string) {
